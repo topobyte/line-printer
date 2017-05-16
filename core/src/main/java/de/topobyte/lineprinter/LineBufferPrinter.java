70240eb51a6fc60e862f0056f11d2f9e1c5936e6
Sebastian Kürten <sebastian@topobyte.de>
2017-05-16 13:39:08 +0200

    Initial sources for core module

---
+++ core/src/main/java/de/topobyte/lineprinter/LineBufferPrinter.java
@@ -0,0 +1,39 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of line-printer.
+//
+// line-printer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// line-printer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with line-printer. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.lineprinter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineBufferPrinter implements LinePrinter
+{
+
+	private List<String> lines = new ArrayList<>();
+
+	@Override
+	public void println(String line)
+	{
+		lines.add(line);
+	}
+
+	public List<String> getLines()
+	{
+		return lines;
+	}
+
+}
---
+++ core/src/main/java/de/topobyte/lineprinter/LinePrinter.java
@@ -0,0 +1,25 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of line-printer.
+//
+// line-printer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// line-printer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with line-printer. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.lineprinter;
+
+public interface LinePrinter
+{
+
+	void println(String line);
+
+}
